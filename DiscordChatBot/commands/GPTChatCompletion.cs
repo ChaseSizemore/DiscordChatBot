1fbee407ec6702e5e4ae1c9fba0cdd1ca84dc3a4
ChaseSizemore <ChaseLSizemore@gmail.com>
2023-11-10 22:40:50 -0500

    gpt

--- DiscordChatBot/commands/GPTChatCompletion.cs
+++ DiscordChatBot/commands/GPTChatCompletion.cs
@@ -1,9 +1,11 @@
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 
 using DotNetEnv;
 using OpenAI;
 using OpenAI_API.Models;
+using DSharpPlus.Interactivity.Extensions;
 
 
 namespace DiscordChatBot.commands
@@ -13,18 +15,32 @@ namespace DiscordChatBot.commands
         [Command("gpt")]
         public async Task GPT(CommandContext ctx, [RemainingText] string message)
         {
-
             string ENV = Env.GetString("OPEN_AI_TOKEN");
 
-            OpenAIAPI api = new OpenAIAPI(ENV);
-            // Console.WriteLine(message);
-            // var result = await api.Completions.CreateCompletionAsync(message);
-            // Console.WriteLine(result.ToString());
-            // var chat = api.Completions
-            // await ctx.Channel.SendMessageAsync(chat.ToString()).ConfigureAwait(false);
+            var api = new OpenAI_API.OpenAIAPI(ENV);
+            var chat = api.Chat.CreateConversation();
+            chat.AppendSystemMessage("You are a discord chat bot tasked with helping users with their problems. Users will ask questions and you will answer them in a helpful and kind way");
 
+            while (true)
+            {
+                chat.AppendUserInput(message);
+                string response = await chat.GetResponseFromChatbotAsync();
+                await ctx.Channel.SendMessageAsync(response.ToString()).ConfigureAwait(false);
+                var nextMessage = await WaitForNextMessageAsync(ctx);
+                if (nextMessage.Content.ToLower() == "end chat")
+                {
+                    await ctx.Channel.SendMessageAsync("Ending chat...");
+                    break;
+                }
+                message = nextMessage.Content;
+            }
+        }
 
-
+        private async Task<DiscordMessage> WaitForNextMessageAsync(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            var result = await interactivity.WaitForMessageAsync(x => x.Channel == ctx.Channel && x.Author == ctx.User).ConfigureAwait(false);
+            return result.Result;
         }
     }
 }
